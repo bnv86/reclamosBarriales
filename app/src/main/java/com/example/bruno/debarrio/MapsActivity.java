d11692765a775d4d37d84bfc2cec5d557c8aacea
bnv86 <vituccibruno@gmail.com>
2018-03-20 12:04:00 -0300

    agregado boton y funcionalidad de mi localizacion en el mapa

--- app/src/main/java/com/example/bruno/debarrio/MapsActivity.java
+++ app/src/main/java/com/example/bruno/debarrio/MapsActivity.java
@@ -1,7 +1,11 @@
 package com.example.bruno.debarrio;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -11,6 +15,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Marker;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -20,12 +25,18 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.security.Permission;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    TextView textview_regresar;
+    double lat =0.0;
+    double lng =0.0;
+    private Marker marcador;
+    //TextView textview_regresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,13 +78,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        //doy los permisos para que se rastree mi ubicacion
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        mMap.setMyLocationEnabled(true); //se habilita mi localizacion
+        miMarcador();
+    }
 
+    //usar un listener para obtener un marcador en donde presiono
+    private void miMarcador() {
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Estoy aquí")); //poner con ubicacion actual
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(getApplicationContext(), "Fue aquí?", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
     }
+/*
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(41.889, -87.622), 16));
+
+        map.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher_foreground))
+                .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+                .position(new LatLng(41.889, -87.622)) //hacer un metodo para hallar la posicion del dispositivo
+                .title("Estoy aquí"));
+    }*/
+/*
+    private void ubicacionPersona() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        updateUbicacionPersona(location);
+        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 15000,0,locListener);
+        LocationListener locationListener = null;
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,10000,0,locationListener);
+
+
+    }
+
+    private void updateUbicacionPersona(Location location) {
+    }*/
 }
