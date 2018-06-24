0039c6f6145e6b31a953d41f98af7b195282ae55
Ale <ale.lopezdmc@gmail.com>
2018-06-23 23:40:17 -0300

    Prueba parceable

--- app/src/main/java/com/example/bruno/debarrio/entidades/Reclamo.java
+++ app/src/main/java/com/example/bruno/debarrio/entidades/Reclamo.java
@@ -1,6 +1,8 @@
 package com.example.bruno.debarrio.entidades;
 
 import android.graphics.Bitmap;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import java.io.Serializable;
 
@@ -8,7 +10,7 @@ import java.io.Serializable;
  * Created by Bruno on 08/04/2018.
  */
 
-public class Reclamo implements Serializable{
+public class Reclamo implements Serializable,Parcelable{
     private String id;
     //private String usuarioDesc;
     private String id_categoria;
@@ -51,6 +53,35 @@ public class Reclamo implements Serializable{
         //this.imagenDescripcion = imagenDescripcion;
     }
 
+    protected Reclamo(Parcel in) {
+        id = in.readString();
+        id_categoria = in.readString();
+        id_usuario = in.readString();
+        id_estado = in.readString();
+        fecha = in.readString();
+        imagen = in.readParcelable(Bitmap.class.getClassLoader());
+        imagenDesc = in.readParcelable(Bitmap.class.getClassLoader());
+        latitudDesc = in.readString();
+        longitudDesc = in.readString();
+        municipalidad = in.readString();
+        descripcionDesc = in.readString();
+        email = in.readString();
+        imagenId = in.readInt();
+        imagenDescripcion = in.readInt();
+    }
+
+    public static final Creator<Reclamo> CREATOR = new Creator<Reclamo>() {
+        @Override
+        public Reclamo createFromParcel(Parcel in) {
+            return new Reclamo(in);
+        }
+
+        @Override
+        public Reclamo[] newArray(int size) {
+            return new Reclamo[size];
+        }
+    };
+
     public String getId() {return id;}
 
     public String getId_categoria(){return id_categoria;}
@@ -89,6 +120,29 @@ public class Reclamo implements Serializable{
 
     public Bitmap getImagenDesc(){return imagenDesc;}
     public void setImagenDesc(Bitmap imagenDesc){this.imagenDesc = imagenDesc;}
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(id_categoria);
+        dest.writeString(id_usuario);
+        dest.writeString(id_estado);
+        dest.writeString(fecha);
+        dest.writeParcelable(imagen, flags);
+        dest.writeParcelable(imagenDesc, flags);
+        dest.writeString(latitudDesc);
+        dest.writeString(longitudDesc);
+        dest.writeString(municipalidad);
+        dest.writeString(descripcionDesc);
+        dest.writeString(email);
+        dest.writeInt(imagenId);
+        dest.writeInt(imagenDescripcion);
+    }
 
     //public int getImagenDescripcion(){return imagenDescripcion;}
     //public void setImagenDescripcion(int imagenDescripcion){this.imagenDescripcion = imagenDescripcion;}
