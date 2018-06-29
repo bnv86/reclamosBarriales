7e184e644acac06dd3e1702875a74b27b8acaf29
bnv86 <vituccibruno@gmail.com>
2018-06-29 13:47:23 -0300

    notificacion push al guardar imagen, al hacer click en la misma redirige a la carpeta donde se alojo

--- app/src/main/java/com/example/bruno/debarrio/fragments/DetalleReclamoFragment.java
+++ app/src/main/java/com/example/bruno/debarrio/fragments/DetalleReclamoFragment.java
@@ -1,6 +1,9 @@
 package com.example.bruno.debarrio.fragments;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -9,8 +12,10 @@ import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -43,6 +48,7 @@ import java.util.Hashtable;
 import java.util.Map;
 import java.util.TreeMap;
 
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 
 /**
@@ -58,6 +64,8 @@ public class DetalleReclamoFragment extends Fragment{ //implements AdapterView.O
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    NotificationCompat.Builder mBuilder;
+    int mNotificationID = 001;
 
 
     // TODO: Rename and change types of parameters
@@ -78,6 +86,7 @@ public class DetalleReclamoFragment extends Fragment{ //implements AdapterView.O
     private TreeMap<String, String> descrip;
     Activity activity;
     ComunicacionFragments interfaceComunicacionFragments;
+    public int PICK_IMAGE_REQUEST = 1;
 
 
     public DetalleReclamoFragment() {
@@ -109,6 +118,7 @@ public class DetalleReclamoFragment extends Fragment{ //implements AdapterView.O
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
@@ -448,6 +458,37 @@ public void subirEstado(String pos){
         void onFragmentInteraction(Uri uri);
     }
 
+    private  void notificationPush() {
+        Bundle bundleObjeto = getArguments();
+        Reclamo reclamo = null;
+        if (bundleObjeto != null) {
+            reclamo = (Reclamo) bundleObjeto.getSerializable("objeto"); //TRAIGO LOS DATOS DEL RECLAMO ACTUAL
+        }
+        //Intent intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        //Intent intentGalleria = new Intent(Intent.ACTION_VIEW, MediaStore.Images.Media.EXTERNAL_CONTENT_URI); ABRE LA GALERIA
+        Intent intentGalleria = new Intent(Intent.ACTION_OPEN_DOCUMENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI); //ABRO LA CARPETA DONDE SE HALLA LA IMAGEN GUARDADA
+        intentGalleria.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        //Intent intentGalleria = new Intent();
+        //intentGalleria.setType("image/*");
+        //intentGalleria.setAction(Intent.ACTION_GET_CONTENT); //android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        //intentGalleria.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.journaldev.com"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(getContext(), 0, intentGalleria, 0);
+        mBuilder = new NotificationCompat.Builder(getContext()).setSmallIcon(android.R.drawable.ic_menu_gallery)
+                .setLargeIcon(reclamo.getImagenDesc()) //TRAIGO LA IMAGEN DEL RECLAMO
+                .setContentTitle("Reclamos Municipales").setContentText("Imagen guardada en galeria/ReclamosMunicipales").setContentIntent(pendingIntent);
+
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(getContext(),
+                        0,
+                        intentGalleria,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getContext().getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(mNotificationID, mBuilder.build());
+    }
+
     private void confirmDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
 
@@ -462,6 +503,7 @@ public void subirEstado(String pos){
                         //guardar imagen
                         Save savefile = new Save();
                         savefile.SaveImage(getContext(), bmap);
+                        notificationPush();
                     }
                 })
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
@@ -472,5 +514,28 @@ public void subirEstado(String pos){
                 })
                 .show();
     }
+
+    /*
+    public void showNotification() {
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+// Podrás mostrar el icono de la notificación, en este caso una alerta
+        Notification notification = new Notification(android.R.drawable.stat_sys_warning,
+                "Notificación", System.currentTimeMillis());
+
+        CharSequence titulo = "Alerta";
+
+// Clase de Notification
+        Intent notificationIntent = new Intent(this, NotificationActivity.class);
+        PendingIntent contIntent = PendingIntent.getActivity(this, , notificationIntent, );
+        notification.setLatestEventInfo(this, "Aviso de notificación", "Esto es un ejemplo de notificación", contIntent);
+
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+//importante
+        int not_id = 1;
+        notificationManager.notify(not_id, notification);
+    }*/
 
 }
