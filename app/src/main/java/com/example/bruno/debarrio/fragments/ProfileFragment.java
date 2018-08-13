4cc3123eca6d8b8da7bf60b314ce56a50b1e5b97
bnv86 <vituccibruno@gmail.com>
2018-08-13 02:29:21 -0300

    se puede actualizar foto de perfil, pero a parte de los demas datos texto de perfil tambien se puso un dialog de confirmacion para cada, con traducciones en, es, it

--- app/src/main/java/com/example/bruno/debarrio/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/bruno/debarrio/fragments/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.example.bruno.debarrio.fragments;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
@@ -13,6 +14,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,6 +33,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.bruno.debarrio.Adapters.AdaptadorReclamos;
 import com.example.bruno.debarrio.HTTP.HttpServices;
 import com.example.bruno.debarrio.MainActivity2;
 import com.example.bruno.debarrio.R;
@@ -84,6 +87,7 @@ public class ProfileFragment extends Fragment {
     ArrayList<String> listaInicio;
     ArrayList<String> listaActualizar;
     ArrayList<Bitmap> listaFoto;
+    ArrayList<Bitmap> listaFotoInicio;
     private String KEY_NOMBRE = "nombre";
     private String KEY_APELLIDO = "apellido";
     private String KEY_EMAIL = "email";
@@ -93,6 +97,7 @@ public class ProfileFragment extends Fragment {
     private String KEY_PASSWORD = "password";
     private String KEY_ID = "id";
     String UPDATE_PROFILE = "https://momentary-electrode.000webhostapp.com/updateProfile.php";
+    String UPDATE_FOTO = "https://momentary-electrode.000webhostapp.com/updateFoto.php";
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -131,6 +136,7 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         final View vista = inflater.inflate(R.layout.fragment_profile, container, false);
         listaInicio = new ArrayList<>();
+        listaFotoInicio = new ArrayList<>();
         editNombre = vista.findViewById(R.id.edit_nombre_perfil);
         editApellido = vista.findViewById(R.id.edit_apellido_perfil);
         editEmail = vista.findViewById(R.id.edit_email_perfil);
@@ -141,6 +147,9 @@ public class ProfileFragment extends Fragment {
         imagenFoto = vista.findViewById(R.id.imagen_para_foto);
         Resources res = getResources();
         Drawable drawable = res.getDrawable(R.drawable.camera);
+
+        GetHttpResponseDatosUser getHttpResponseDatosUser = new GetHttpResponseDatosUser(getContext());
+        getHttpResponseDatosUser.execute();
 
         botonCambiarFoto = vista.findViewById(R.id.boton_foto_perfil);
 
@@ -156,6 +165,7 @@ public class ProfileFragment extends Fragment {
         }
 
         botonActualizar = vista.findViewById(R.id.boton_actualizar_perfil);
+
         botonActualizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -165,18 +175,22 @@ public class ProfileFragment extends Fragment {
                 String telefonoInicio = editTelefono.getText().toString();
                 String usernameInicio = editUsuario.getText().toString();
                 String passwordInicio = editPassword.getText().toString();
+                //String fotoInicio = getStringImagen(bitmap);
+                //Bitmap fotoInicioConver = downloadImage(fotoInicio);
 
-                listaActualizar.add(nombreInicio);
-                listaActualizar.add(apellidoInicio);
-                listaActualizar.add(emailInicio);
-                listaActualizar.add(telefonoInicio);
-                listaActualizar.add(usernameInicio);
-                listaActualizar.add(passwordInicio);
-                actualizarPerfil(listaInicio);
+                listaInicio.add(nombreInicio);
+                listaInicio.add(apellidoInicio);
+                listaInicio.add(emailInicio);
+                listaInicio.add(telefonoInicio);
+                listaInicio.add(usernameInicio);
+                listaInicio.add(passwordInicio);
+                confirmDialogPerfil(listaInicio);
+
+                //actualizarPerfil(listaActualizar);
+                //actualizarFoto(fotoInicioConver);
             }
         });
-        GetHttpResponseDatosUser getHttpResponseDatosUser = new GetHttpResponseDatosUser(getContext());
-        getHttpResponseDatosUser.execute();
+        listaInicio.clear();
 
         return vista;
     }
@@ -211,12 +225,57 @@ public class ProfileFragment extends Fragment {
             try {
                 //obtener el mapa de bits de la galería
                 bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), filePath);
+                confirmDialogFoto(bitmap);
                 //se setea la foto en lugar de la imagenView predeterminada
-                imagenFoto.setImageBitmap(bitmap);
+                //imagenFoto.setImageBitmap(bitmap);
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private void confirmDialogFoto(final Bitmap bitmap) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext()); //getResources().getString(R.string.desasociar_reclamo)
+
+        builder.setMessage(getResources().getString(R.string.confirmacion_actualizar_foto))
+                .setPositiveButton(getResources().getString(R.string.str_confirmar),  new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        imagenFoto.setImageBitmap(bitmap);
+                        actualizarFoto(bitmap);
+                        //Intent intent = new Intent(getActivity(), MainActivity2.class);
+                        //startActivity(intent);
+                    }
+                })
+                .setNegativeButton(getResources().getString(R.string.str_cancelar), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog,int id) {
+                        dialog.cancel();
+                    }
+                })
+                .show();
+    }
+
+    private void confirmDialogPerfil(final ArrayList<String> listaUp) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext()); //getResources().getString(R.string.desasociar_reclamo)
+
+        builder.setMessage(getResources().getString(R.string.confirmacion_actualizar_perfil))
+                .setPositiveButton(getResources().getString(R.string.str_confirmar),  new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        actualizarPerfil(listaUp);
+                        //Intent intent = new Intent(getActivity(), MainActivity2.class);
+                        //startActivity(intent);
+                    }
+                })
+                .setNegativeButton(getResources().getString(R.string.str_cancelar), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog,int id) {
+                        dialog.cancel();
+                    }
+                })
+                .show();
     }
 
     public void actualizarPerfil(ArrayList<String> listaUpdate){
@@ -232,6 +291,7 @@ public class ProfileFragment extends Fragment {
         final String username = listaUpdate.get(4).toString();
         final String password = listaUpdate.get(5).toString();
         //String foto = lista.get(0).toString();
+        listaUpdate.clear();
 
         //final ProgressDialog loading = ProgressDialog.show(getActivity(),"Actualizando...","Espere por favor...",false,false); //getActivity()
         StringRequest stringRequest = new StringRequest(Request.Method.POST, UPDATE_PROFILE,
@@ -240,14 +300,14 @@ public class ProfileFragment extends Fragment {
                     public void onResponse(String s) {
                         //Descartar el diálogo de progreso
                         //loading.dismiss();
-                        Toast.makeText(getActivity(), "PERFIL ACTUALIZADO! "+id, Toast.LENGTH_LONG).show();
+                        Toast.makeText(getActivity(), getResources().getString(R.string.perfil_actualizado), Toast.LENGTH_LONG).show();
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError volleyError) {
                         //loading.dismiss();
-                        Toast.makeText(getActivity(), "NO SE ACTUALIZÓ...REINTENTE" , Toast.LENGTH_LONG).show();
+                        Toast.makeText(getActivity(), getResources().getString(R.string.str_reintente) , Toast.LENGTH_LONG).show();
                     }
                 }){
 
@@ -260,11 +320,60 @@ public class ProfileFragment extends Fragment {
                 params.put(KEY_NOMBRE, nombre);
                 params.put(KEY_APELLIDO, apellido);
                 params.put(KEY_EMAIL, email);
-                //params.put(KEY_TELEFONO, phone+"");
                 params.put(KEY_TELEFONO, p+"");
-                //params.put(KEY_FOTO, foto);
                 params.put(KEY_USERNAME, username);
                 params.put(KEY_PASSWORD, password);
+                //Parámetros de retorno
+                return params;
+            }
+        };
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(0, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //Creación de una cola de solicitudes
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext()); //getActivity()
+        //Agregar solicitud a la cola
+        requestQueue.add(stringRequest);
+    }
+
+    public void actualizarFoto(Bitmap fotoActualizada){
+        SharedPreferences sharedpreferences = getActivity().getSharedPreferences("sesion", getActivity().getApplication().MODE_PRIVATE);
+        final String id = sharedpreferences.getString("id_usuario","");
+        final String foto = getStringImagen(fotoActualizada);
+        //String foto = lista.get(0).toString();
+        //final ProgressDialog loading = ProgressDialog.show(getActivity(),"Actualizando...","Espere por favor...",false,false); //getActivity()
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, UPDATE_FOTO,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        //Descartar el diálogo de progreso
+                        //loading.dismiss();
+                        Toast.makeText(getActivity(), getResources().getString(R.string.foto_actualizada), Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        //loading.dismiss();
+                        Toast.makeText(getActivity(), getResources().getString(R.string.str_reintente) , Toast.LENGTH_LONG).show();
+                    }
+                }){
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //Creación de parámetros
+                Map<String,String> params = new Hashtable<String, String>();
+                //Agregando de parámetros
+                params.put(KEY_ID, id);
+                params.put(KEY_FOTO, foto);
+                /*
+                if (fotoLista == null){
+                    final String foto1 = getStringImagen(bitmap);
+                    params.put(KEY_FOTO, foto1);
+                }
+                else{
+                    final String foto2 = getStringImagen(fotoLista);
+                    params.put(KEY_FOTO, foto2);
+                }*/
+
                 //Parámetros de retorno
                 return params;
             }
@@ -326,13 +435,11 @@ public class ProfileFragment extends Fragment {
                     if (ResultHolder != null){
                         JSONArray jsonArray = null;
                         try {
-                            //listaDatosUser = new ArrayList<>();
                             lista = new ArrayList<>();
                             listaFoto = new ArrayList<>();
                             jsonArray = new JSONArray(ResultHolder);
                             JSONObject jsonObject;
                             SharedPreferences sharedpreferences = getActivity().getSharedPreferences("sesion", getActivity().getApplication().MODE_PRIVATE);
-                            //SharedPreferences prefReclamo = getContext().getSharedPreferences("reclamo", getActivity().MODE_PRIVATE);
                             String id_usuario = sharedpreferences.getString("id_usuario","");
                             for (int i=0; i<jsonArray.length();i++){
                                 jsonObject= jsonArray.getJSONObject(i);
@@ -352,9 +459,6 @@ public class ProfileFragment extends Fragment {
                                     editor.putString("foto_usuario", dec); //GUARDA LA IMAGEN PARA USAR EN APP BAR
                                     editor.commit();
 
-                                    //Perfil perfil = new Perfil(id_usuario.toString(), nombre.toString(), apellido.toString(), email.toString(), telefono.toString(),
-                                    //        username.toString(), password.toString()); //foto.toString()
-                                    //listaDatosUser.add(perfil);
                                     lista.add(nombre.toString());
                                     lista.add(apellido.toString());
                                     lista.add(email.toString());
@@ -396,9 +500,8 @@ public class ProfileFragment extends Fragment {
                 editPassword.setText(lista.get(5).toString(), TextView.BufferType.EDITABLE);
                 imagenFoto.setImageBitmap(listaFoto.get(0));
 
-
                 listaActualizar = new ArrayList<>();
-
+                /*
                 botonActualizar.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -408,6 +511,7 @@ public class ProfileFragment extends Fragment {
                         String telefonoActualizado = editTelefono.getText().toString();
                         String usernameActualizado = editUsuario.getText().toString();
                         String passwordActualizado = editPassword.getText().toString();
+                        //Bitmap fotoActualizada = listaFoto.get(0);
 
                         listaActualizar.add(nombreActualizado);
                         listaActualizar.add(apellidoActualizado);
@@ -415,11 +519,18 @@ public class ProfileFragment extends Fragment {
                         listaActualizar.add(telefonoActualizado);
                         listaActualizar.add(usernameActualizado);
                         listaActualizar.add(passwordActualizado);
-                        actualizarPerfil(listaActualizar);
+
+                        //if (bitmap == null){
+                        //    actualizarPerfil(listaActualizar, bitmap);
+                        //}
+                        //else{
+                        //    actualizarPerfil(listaActualizar, fotoActualizada);
+                        //}
+
                         lista.clear();
                         listaActualizar.clear();
                     }
-                });
+                });*/
                 //String fot = lista.get(6).toString();
                 //Bitmap foto = downloadImage(fot);
                 //imagenFoto.setImageBitmap(foto);
